95073a5ab65cae7f24aa0535f31baf122cc53879
oabuhassan <oabuhassan@sdaia.gov.sa>
2025-04-15 21:51:37 +0300

    fix seat_id to make it unique

--- backend/db/init.py
+++ backend/db/init.py
@@ -26,18 +26,40 @@ add_record(matches_db, matches_fields, {'match_id': '2', 'team1_name': 'Al-Nasr'
 add_record(matches_db, matches_fields, {'match_id': '3', 'team1_name': 'Al-Faisaly', 'team2_name': 'Al-Taawoun', 'number_of_seats': '15'})
 
 
-# for loop to add seats 15% vip, 25% regular, 60% economy to all matches based on number of seats
+seat_id_counter = 1  # Start global seat ID counter
+
 for match in read_all(matches_db):
     match_id = match['match_id']
     number_of_seats = int(match['number_of_seats'])
+
     vip_seats = int(number_of_seats * 0.15)
     regular_seats = int(number_of_seats * 0.25)
     economy_seats = number_of_seats - vip_seats - regular_seats
 
-    for i in range(vip_seats):
-        add_record(seats_db, seats_fields, {'seat_id': f'{i+1}', 'match_id': match_id, 'catagory': 'VIP', 'occupied': 'False'})
-    for i in range(regular_seats):
-        add_record(seats_db, seats_fields, {'seat_id': f'{i+1}', 'match_id': match_id, 'catagory': 'Regular', 'occupied': 'False'})
-    for i in range(economy_seats):
-        add_record(seats_db, seats_fields, {'seat_id': f'{i+1}', 'match_id': match_id, 'catagory': 'Economy', 'occupied': 'False'})
+    for _ in range(vip_seats):
+        add_record(seats_db, seats_fields, {
+            'seat_id': str(seat_id_counter),
+            'match_id': match_id,
+            'catagory': 'VIP',
+            'occupied': 'False'
+        })
+        seat_id_counter += 1
+
+    for _ in range(regular_seats):
+        add_record(seats_db, seats_fields, {
+            'seat_id': str(seat_id_counter),
+            'match_id': match_id,
+            'catagory': 'Regular',
+            'occupied': 'False'
+        })
+        seat_id_counter += 1
+
+    for _ in range(economy_seats):
+        add_record(seats_db, seats_fields, {
+            'seat_id': str(seat_id_counter),
+            'match_id': match_id,
+            'catagory': 'Economy',
+            'occupied': 'False'
+        })
+        seat_id_counter += 1
 
