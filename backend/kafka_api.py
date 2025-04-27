7fa482e791ffd68cc2ebbe8a54126aa9373f0cad
oabuhassan <oabuhassan@sdaia.gov.sa>
2025-04-27 19:24:27 +0300

    init kafka api

--- backend/kafka_api.py
+++ backend/kafka_api.py
@@ -7,7 +7,7 @@ app = FastAPI()
 
 # Kafka Producer configuration
 producer_config = {
-    'bootstrap.servers': 'localhost:9092'  # Update as needed
+    'bootstrap.servers': 'localhost:39092'  # Update as needed
 }
 producer = Producer(producer_config)
 
@@ -18,13 +18,7 @@ def delivery_report(err, msg):
         print(f"Record {msg.key()} successfully produced to {msg.topic()} [{msg.partition()}] at offset {msg.offset()}")
 
 @app.post("/publish")
-async def publish_message(request: Request):
-    body = await request.json()
-
-    # Extract topic and message
-    topic = body.get("topic")
-    request_id = body.get("request_id")
-    username = body.get("username")
+async def publish_message(request: Request, topic: str = None, request_id: str = None, username: str = None):
 
     # create json message containing request_id and username
     message = {
@@ -41,3 +35,16 @@ async def publish_message(request: Request):
         return {"status": "success", "topic": topic, "message": message}
     except Exception as e:
         return {"status": "error", "details": str(e)}
+    
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(
+        "kafka_api:app",      # "module:variable"
+        host="0.0.0.0",
+        port=8009,
+        reload=True,        # hot-reload on file changes (dev only)
+        log_level="info"
+    )
+
